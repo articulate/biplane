a33faa489953ad0f64d8ad5378a7b4900aae146b
Luke van der Hoeven <hungerandthirst@gmail.com>
2016-03-16 15:18:47 -0400

    add additional specs

--- spec/route_spec.cr
+++ spec/route_spec.cr
@@ -4,6 +4,15 @@ module Biplane
   describe Route do
     route = Route.new("/one/:one/:two")
 
+    it "can partially apply args" do
+      route.to_s.should eq "/one/:one/:two"
+      route.to_s({two: 2}).should eq "/one/:one/2"
+    end
+
+    it "ignores unknown path parts" do
+      route.to_s({game: "on"}).should eq "/one/:one/:two"
+    end
+
     it "is idempotent" do
       route.to_s({one: 1}).should eq "/one/1/:two"
       route.to_s({one: 1}).should eq "/one/1/:two"
