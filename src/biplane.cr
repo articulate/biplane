f8a70ac47047ea944aaa1f41d573d48e984fb7be
Luke van der Hoeven <hungerandthirst@gmail.com>
2016-04-26 10:36:18 -0400

    fix local build issue
    see https://github.com/crystal-lang/crystal/issues/2515

--- src/biplane.cr
+++ src/biplane.cr
@@ -1,3 +1,10 @@
+# Required here because of a bug in Crystal
+# See: https://github.com/crystal-lang/crystal/issues/2515
+#
+# Should be moved back to interpolate.cr
+#
+require "crustache"
+
 require "./biplane/*"
 
 module Biplane
--- src/biplane/interpolate.cr
+++ src/biplane/interpolate.cr
@@ -1,15 +1,13 @@
-require "crustache"
-
 module Biplane
   class Interpolate
     getter :template
 
-    def self.new(path : String)
-      new File.open(path)
-    end
-
     def initialize(io : IO)
       @template = Crustache.parse io.gets_to_end
+    end
+
+    def initialize(path : String)
+      @template = Crustache.parse File.read(path)
     end
 
     # assumes all keys are string
