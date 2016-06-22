c82728222e91bdb3a6a7e5353b7e0dfb848777b7
Brian Stolz <brian@tecnobrat.com>
2016-06-22 12:47:57 -0700

    Try using the "now" string which postgres handles.
    Postgres should turn this into the current epoch automatically.

--- src/biplane/configs/api_config.cr
+++ src/biplane/configs/api_config.cr
@@ -36,7 +36,7 @@ module Biplane
         "request_path":       request_path,
         "strip_request_path": strip_request_path,
         "upstream_url":       upstream_url,
-        "created_at":         Time.now.epoch,
+        "created_at":         "now",
       }
     end
 
